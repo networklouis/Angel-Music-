bafe54e99970cd3060fb40dce769d26452ec2b88
networklouis <louis84385@gmail.com>
2025-03-09 21:37:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/networklouis/Angel-music",
+    "https://github.com/networklouis/Angel-Music-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
